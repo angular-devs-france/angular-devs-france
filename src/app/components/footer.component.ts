5ae07b99d44b322f3cf73fd6fc347693490ce755
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-09-11 17:29:49 +0200

    feat: add spacing for mobile touch interaction

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -12,25 +12,25 @@ import {RouterLink} from "@angular/router";
       <section>
         <h4 class="font-bold text-xl mb-2">Communauté</h4>
         <ul>
-          <li><a href="https://twitter.com/AngularDevsFr" target="_blank">Twitter</a></li>
-          <li><a href="https://www.linkedin.com/company/angular-devs-france" target="_blank">LinkedIn</a></li>
-          <li><a href="https://discord.gg/c8XThfW9a7" target="_blank">Discord</a></li>
+          <li class="py-1 sm:py-0"><a href="https://twitter.com/AngularDevsFr" target="_blank">Twitter</a></li>
+          <li class="py-1 sm:py-0"><a href="https://www.linkedin.com/company/angular-devs-france" target="_blank">LinkedIn</a></li>
+          <li class="py-1 sm:py-0"><a href="https://discord.gg/c8XThfW9a7" target="_blank">Discord</a></li>
         </ul>
       </section>
 
       <section>
         <h4 class="font-bold text-xl mb-2">Evènements</h4>
         <ul>
-          <li><a href="https://www.youtube.com/channel/UCmEGS2U5CSzWBt62WnCh_Cw" target="_blank">YouTube</a></li>
-          <li><a routerLink="/code-de-conduite">Code de conduite</a></li>
+          <li class="py-1 sm:py-0"><a href="https://www.youtube.com/channel/UCmEGS2U5CSzWBt62WnCh_Cw" target="_blank">YouTube</a></li>
+          <li class="py-1 sm:py-0"><a routerLink="/code-de-conduite">Code de conduite</a></li>
         </ul>
       </section>
 
       <section>
         <h4 class="font-bold text-xl mb-2">Nous rencontrer</h4>
         <ul>
-          <li><a href="" target="_blank">Proposer un sujet</a></li>
-          <li><a href="mailto:angulardevs.fr">Nous contacter</a></li>
+          <li class="py-1 sm:py-0"><a href="" target="_blank">Proposer un sujet</a></li>
+          <li class="py-1 sm:py-0"><a href="mailto:angulardevs.fr">Nous contacter</a></li>
         </ul>
       </section>
     </footer>
